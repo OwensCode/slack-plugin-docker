ec9f73e026a8d400049859858bd1f7f75d443870
Owen Berry <owen.berry@hs2solutions.com>
2015-05-04 09:22:14 -0400

    Run Maven install phase to execute all plugins

--- build-plugin.sh
+++ build-plugin.sh
@@ -4,5 +4,5 @@ docker run --rm -t -i \
     -v $PWD/../slack-plugin:/slack-plugin \
     -v $PWD/../slack-plugin-docker/m2repo:/var/m2repo \
     slack-plugin-build \
-    mvn clean package
+    mvn clean install
 
